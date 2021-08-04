0ac87a4fd11d2a96d80e5b03195f3cd3e45392ac
Libby Lebyane <lebyanelm@nextify.co.za>
2021-08-04 13:38:13 +0200

    Added a syncing and identity state to poem input

--- src/components/PoemPostInput/PoemPostInput.tsx
+++ src/components/PoemPostInput/PoemPostInput.tsx
@@ -1,29 +1,52 @@
-import React, { useContext } from 'react';
+import React, { Component } from 'react';
 import styles from './PoemPostInput.module.scss';
-import { SessionContext } from '../../App';
 import IonIcon from '@reacticons/ionicons';
+import { BeatLoader } from 'react-spinners';
 
-const PoemPostInput = () => {
-  return (
-    <div className={styles.PoemInputContainer}>
-      <input className={styles.PoemInputTitle} placeholder="Type a poem title here..." />
-      <textarea className={styles.PeomInput} placeholder="Craft your poem in this space, resize height if needed."></textarea>
+class PoemPostInput extends Component {
+  state = {
+    isAnonymousPublish: false
+  }
 
-      
-
-      <div className={styles.Buttons}>
-        <button>
-          <span>Attach Thumbnail</span>
-          <IonIcon name="attach"></IonIcon>
-        </button>
-
-        <button>
-          <span>Publish Poem</span>
-          <IonIcon name="arrow-forward"></IonIcon>
-        </button>
+  toggleIdentity = () => {
+    this.state.isAnonymousPublish = !this.state.isAnonymousPublish;
+    this.setState({ ...this.state });
+  }
+  
+  render() {
+    return (
+      <div className={styles.PoemInputContainer}>
+        <input className={styles.PoemInputTitle} placeholder="Type a poem title here..." />
+        <textarea className={styles.PeomInput} placeholder="Craft your poem in this space, resize height if needed."></textarea>
+  
+        
+  
+        <div className={styles.Buttons}>
+          <section>
+            <button className="outline">
+              <span>Attach Thumbnail</span>
+              <IonIcon name="attach"></IonIcon>
+            </button>
+  
+            <button>
+              <span>Publish Poem</span>
+              <IonIcon name="arrow-forward"></IonIcon>
+            </button>
+          </section>
+          <section>
+            <a className={styles.PostingAuthority} onClick={this.toggleIdentity}>
+              {this.state.isAnonymousPublish ? 'Anonymous' : 'Publishing as Libby Lebyane'}
+            </a>
+            &mdash;
+  
+            {/* When the user updates the poem draft */}
+            <BeatLoader></BeatLoader>
+            <span className={styles.SyncingStatus}>Syncing draft (<a>Delete</a>)</span>
+          </section>
+        </div>
       </div>
-    </div>
-  )
-};
+    )
+  }
+}
 
 export default PoemPostInput;
