ba1fa7418107c3c06d492af06d895ef5b1e6b828
Libby Lebyane <lebyane.lm@gmail.com>
2021-12-01 22:45:38 +0200

    Removed testing environment from config.js

--- src/config.js
+++ src/config.js
@@ -12,21 +12,10 @@ const production = {
   FRONTEND: "https://rebbelpoetry.com",
 };
 
-// Environment for testing values
-const testing = {
-  ENV: "testing",
-  BACKEND: "http://localhost:5000/api",
-  FRONTEND: "http://localhost:3000",
-};
-
 // Environment for all values - doesn't depend on any environment it's always there
 const config = {
   APP_NAME: "Rebbel Poetry",
-  ...(process.env.NODE_ENV === "development"
-    ? developmnent
-    : process.env.NODE_ENV === "production"
-    ? production
-    : testing),
+  ...(process.env.NODE_ENV === "development" ? developmnent : production),
 };
 
 export default config;
